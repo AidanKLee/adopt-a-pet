fb8d29a470fe483f6b878e9aaffe27e70e754b34
Aidan Lee <aidankglee@googlemail.com>
2022-03-02 21:10:38 +0000

    First commit.

---
+++ src/pages/home/index.js
@@ -0,0 +1,45 @@
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { getPets } from '../../api/petfinder';
+import Hero from '../../components/hero';
+import Pet from '../../components/pet';
+
+const HomePage = () => {
+  const [data, setData] = useState(null);
+  const { type } = useParams(); // Fix me!
+
+  useEffect(() => {
+    async function getPetsData() {
+      const petsData = await getPets(type);
+      setData(petsData);
+    }
+
+    getPetsData();
+  }, [type]);
+
+  if (!data) {
+    return <h2>Loading...</h2>;
+  }
+
+  return (
+    <div className="page">
+      <Hero />
+      <h3>
+        <span className="pet-type-label">{type ? `${type}s` : 'Pets'}</span>{' '}
+        available for adoption near you
+      </h3>
+
+      {data.length ? (
+        <div className="grid">
+          {data.map((animal) => (
+            <Pet animal={animal} /> // Don't forget to change me!
+          ))}
+        </div>
+      ) : (
+        <p className="prompt">No {type}s available for adoption now.</p>
+      )}
+    </div>
+  );
+};
+
+export default HomePage;
